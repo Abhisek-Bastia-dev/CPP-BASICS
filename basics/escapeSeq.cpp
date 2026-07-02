99f1eadebcea38ced49111a9fe0f06236a84e922
Abhisek-Bastia-dev <simplecodeabhi@gmail.com>
2026-07-02 16:14:44 +0530

    Learned basic program, escape sequence

---
+++ basics/escapeSeq.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+  // \n and <<endl
+
+  cout<<"Hello\nCPP";
+  cout<<endl<<"Object"<<endl<<"Arrays";
+  cout<<endl<<endl<<"Object"<<endl<<endl<<"Arrays\n";
+  cout<<"nn\n\nnn\n";
+  cout<<"nn/n/nnn/n";
+
+  cout<<endl<< 4;
+  cout<<endl<< 4 + 2;
+  cout<<endl<<"4 + 2";
+
+  return 0;
+}
